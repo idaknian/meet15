40f1a80e9ac29df5ab30260a51020314b44f4e64
Codings01 <bernardlii4225@gmail.com>
2023-11-02 16:23:23 +0700

    Second Commi

--- application/routes.py
+++ application/routes.py
@@ -2,13 +2,14 @@ from flask import render_template, redirect, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
 
 from application import app
-from application.forms import User
-from application.models import LoginForm
+from application.models import *
+from application.forms import *
+from application.utils import save_image
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if curret_user.is_authenticated:
-        return redirect(url_for('protected'))
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
 
     form = LoginForm()
 
@@ -19,11 +20,11 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and password == user.password:
             login_user(user)
-            return redirect(url_for('protected'))
+            return redirect(url_for('profile'))
         else:
-            flash("Invalid username or password", 'error')
+            flash('Invalid username or password', 'error')
 
-    return render_template('login.html', form=form)
+    return render_template('login.html', title="Login", form=form)
 
 @app.route('/logout')
 @login_required
@@ -31,10 +32,44 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-@app.route('/protected')
+@app.route('/profile')
 @login_required
-def protected():
-    return 'This is a protected page. You are logged in as ' + curredt_user.username
+def profile():
+    return render_template('profile.html', title=f'{current_user.fullname} Profile')
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required
+def index():
+    form = CreatePostForm()
+
+    if form.validate_on_submit():
+        post = Post(
+            author_id = current_user.id,
+            caption = form.caption.data
+        )
+        post.photo = save_image(form.post_pic.data)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your Image has been posted 📮!', 'success')
+
+    posts = Post.query.filter_by(author_id = current_user.id).all()
+
+    return render_template('index.html', title='Home', form=form, posts=posts)
+
+@app.route('/signup')
+def signup():
+    form = SignUpForm()
+    return render_template('signup.html', title='SignUp', form=form)
+
+# @app.route('/editprofile')
+# def editprofile():
+#     form = EditProfileForm()
+#     return render_template('editprofile.html', title='EditProfile')
+
+@app.route('/about')
+@login_required
+def about():
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
